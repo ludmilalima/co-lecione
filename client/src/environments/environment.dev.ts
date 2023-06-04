ab67edc0f49898557fb95469547c3cb8efda12b2
ludmilalima <ludmilalima@outlook.com>
2023-06-04 20:17:32 -0300

    Configura variáveis de ambiente

---
+++ client/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'http://localhost:5200', // URL base para o ambiente de desenvolvimento
+  };
+  
---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://api.example.com/api', // URL base para o ambiente de produção
+  };
+  
