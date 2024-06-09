ffab9331fa5962032dfaaa10d19daa94eea010d9
ludmilalima <ludmilalima@outlook.com>
2024-06-09 15:03:33 -0300

    create question component

---
+++ client/src/app/components/new-question/new-question.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-question',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-question.component.html',
+  styleUrl: './new-question.component.scss'
+})
+export class NewQuestionComponent {
+
+}
