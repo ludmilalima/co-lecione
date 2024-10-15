cca844181389ef398749dcea3d5b828521ffc529
ludmilalima <ludmilalima@outlook.com>
2024-10-15 14:54:56 -0300

    debbuging

--- server/src/server/routes/user.routes.ts
+++ server/src/server/routes/user.routes.ts
@@ -17,6 +17,8 @@ userRouter.use(express.json());
  *         description: A list of users.
  */
 userRouter.get('/', async (_req, res) => {
+  console.log(_req);
+  console.log(res);
   try {
     const users = await UserModel.find({});
     res.status(200).json(users);
