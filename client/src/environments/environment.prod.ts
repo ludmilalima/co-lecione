d9c0a31a107f143755f7439b99d1400ac0ce90a8
ludmilalima <ludmilalima@outlook.com>
2024-10-15 15:13:17 -0300

    remove port from url, clean console.log()

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://backend-wyl7.onrender.com:5200', // URL base para o ambiente de produção
+  baseUrl: 'https://backend-wyl7.onrender.com', // URL base para o ambiente de produção
 };
