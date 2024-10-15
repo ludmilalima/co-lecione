b6cc980c8ecb85d7dbc141600818b9761b19ae6e
ludmilalima <ludmilalima@outlook.com>
2024-10-15 16:44:23 -0300

    update baseUrl reference

--- client/src/app/shared/services/email-sender.service.ts
+++ client/src/app/shared/services/email-sender.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class EmailSenderService {
 
-    private apiUrl = 'http://localhost:5200/api/send-email'; // URL do endpoint do backend
+    private apiUrl = environment.baseUrl + '/api/send-email'; // URL do endpoint do backend
 
     constructor(private http: HttpClient) { }
 
