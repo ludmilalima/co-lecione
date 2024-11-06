7680a3bdd803e3d8a05d49c4a638c70ab38abe5e
Bruno Sampaio <bruno.sampaio08@unifesp.br>
2024-11-05 21:41:29 -0300

    update filter values representation

--- client/src/app/components/reusable/filter/custom-type-filter/custom-type-filter.component.ts
+++ client/src/app/components/reusable/filter/custom-type-filter/custom-type-filter.component.ts
@@ -1,18 +1,25 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
-import { NodeInfo } from 'src/app/core/models/metadata/util.model';
-import { SimpleTextInputComponent } from '../simple-text-input/simple-text-input.component';
-import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { FormsModule } from '@angular/forms';
-import { NotificationsService } from 'src/app/shared/notifications/notifications.service';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from "@angular/core";
+import { NodeInfo } from "src/app/core/models/metadata/util.model";
+import { SimpleTextInputComponent } from "../simple-text-input/simple-text-input.component";
+import { CommonModule } from "@angular/common";
+import { MatIconModule } from "@angular/material/icon";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+import { NotificationsService } from "src/app/shared/notifications/notifications.service";
 
 @Component({
-  selector: 'app-custom-type-filter',
+  selector: "app-custom-type-filter",
   standalone: true,
   imports: [
     CommonModule,
@@ -27,17 +34,21 @@ import { NotificationsService } from 'src/app/shared/notifications/notifications
     MatTableModule,
     MatButtonModule,
   ],
-  templateUrl: './custom-type-filter.component.html',
-  styleUrl: './custom-type-filter.component.scss'
+  templateUrl: "./custom-type-filter.component.html",
+  styleUrl: "./custom-type-filter.component.scss",
 })
 export class CustomTypeFilterComponent implements OnInit, OnChanges {
   @Input() filterComponent: Array<any>;
   @Input() clearFilters: boolean;
 
-  @ViewChild('keySimpleTextInputComponent', { read: SimpleTextInputComponent }) keySimpleTextInputComponent: SimpleTextInputComponent;
-  @ViewChild('valueSimpleTextInputComponent', { read: SimpleTextInputComponent }) valueSimpleTextInputComponent: SimpleTextInputComponent;
+  @ViewChild("keySimpleTextInputComponent", { read: SimpleTextInputComponent })
+  keySimpleTextInputComponent: SimpleTextInputComponent;
+  @ViewChild("valueSimpleTextInputComponent", {
+    read: SimpleTextInputComponent,
+  })
+  valueSimpleTextInputComponent: SimpleTextInputComponent;
 
-  constructor(private _notificationsService: NotificationsService) { }
+  constructor(private _notificationsService: NotificationsService) {}
 
   keyObj: CustomType = new CustomType();
   valueObj: CustomType = new CustomType();
@@ -45,29 +56,29 @@ export class CustomTypeFilterComponent implements OnInit, OnChanges {
   key: string | null = null;
   value: string | null = null;
 
-  displayedColumns: string[] = ['key', 'value', 'actions'];
+  displayedColumns: string[] = ["key", "value", "actions"];
   dataSource = new MatTableDataSource<any>();
 
   ngOnInit() {
-    this.keyObj.nodeInfo.key = 'Chave';
-    this.valueObj.nodeInfo.key = 'Valor';
-    this.keyObj.nodeInfo.description = 'Insert the key of the pair.';
-    this.valueObj.nodeInfo.description = 'Insert the value of the pair.';
+    this.keyObj.nodeInfo.key = "Chave";
+    this.valueObj.nodeInfo.key = "Valor";
+    this.keyObj.nodeInfo.description = "Insert the key of the pair.";
+    this.valueObj.nodeInfo.description = "Insert the value of the pair.";
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes['clearFilters'] && changes['clearFilters'].currentValue) {
+    if (changes["clearFilters"] && changes["clearFilters"].currentValue) {
       this.clearFilter();
     }
   }
 
   onKeyChange(value: string) {
     this.key = value;
-  };
+  }
 
   onValueChange(value: string) {
     this.value = value;
-  };
+  }
 
   addFilterPair() {
     if (this.key && this.value) {
@@ -75,22 +86,26 @@ export class CustomTypeFilterComponent implements OnInit, OnChanges {
       result.nodeInfo.key = this.key;
       result.value = this.value;
 
-      this.dataSource.data = [...this.dataSource.data, { key: this.key, value: this.value }];
+      //this.dataSource.data = [...this.dataSource.data, { key: this.key, value: this.value }];
 
-      let item = this.filterComponent.find(item => item.nodeInfo.key === result.nodeInfo.key);
+      let item = this.filterComponent.find(
+        (item) => item.nodeInfo.key === result.nodeInfo.key
+      );
       if (item == undefined) {
         this.filterComponent.push(result);
-      }
-      else {
+      } else {
         this.filterComponent[this.filterComponent.indexOf(item)] = result;
       }
 
       this.clearInput();
+    } else {
+      this._notificationsService.error(
+        "Erro!",
+        "A chave e o valor são obrigatórios.",
+        5000
+      );
     }
-    else {
-      this._notificationsService.error('Erro!', 'A chave e o valor são obrigatórios.', 5000);
-    }
-  };
+  }
 
   clearInput() {
     this.keySimpleTextInputComponent.reset();
@@ -101,20 +116,19 @@ export class CustomTypeFilterComponent implements OnInit, OnChanges {
     this.valueObj.value = null;
   }
 
-  removeFilterPair(index: number) {
-    let obj = this.dataSource.data[index];
+  // removeFilterPair(index: number) {
+  //   let obj = this.dataSource.data[index];
 
-    this.dataSource.data = this.dataSource.data.filter((_, i) => i !== index);
+  //   this.dataSource.data = this.dataSource.data.filter((_, i) => i !== index);
 
-    let item = this.filterComponent.find(item => item.nodeInfo.key === obj.key);
-    this.filterComponent.splice(this.filterComponent.indexOf(item), 1);
-  };
+  //   let item = this.filterComponent.find(item => item.nodeInfo.key === obj.key);
+  //   this.filterComponent.splice(this.filterComponent.indexOf(item), 1);
+  // };
 
   clearFilter() {
     this.dataSource.data = [];
     this.clearInput();
   }
-
 }
 
 class CustomType {
@@ -125,8 +139,8 @@ class CustomType {
     this.nodeInfo = new NodeInfo(
       0,
       20,
-      'A string of characters.',
-      'custom-type'
+      "A string of characters.",
+      "custom-type"
     );
   }
 }
--- client/src/app/components/reusable/metadata-form/metadata-form.component.ts
+++ client/src/app/components/reusable/metadata-form/metadata-form.component.ts
@@ -1,14 +1,31 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTabsModule } from '@angular/material/tabs';
-import { CardsComponent } from 'src/app/components/reusable/cards/cards.component';
-import { FilterComponent } from 'src/app/components/reusable/filter/filter.component';
-import { QuestionComponent } from 'src/app/components/reusable/question/question.component';
+import { CommonModule } from "@angular/common";
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  IterableDiffer,
+  IterableDiffers,
+  OnInit,
+  Output,
+  ViewChild,
+} from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from "@angular/material/button";
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatSort, MatSortModule } from "@angular/material/sort";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { CardsComponent } from "src/app/components/reusable/cards/cards.component";
+import { FilterComponent } from "src/app/components/reusable/filter/filter.component";
+import { QuestionComponent } from "src/app/components/reusable/question/question.component";
+import { ProcessMetadataService } from "../filter/process-metadata.service";
 
 @Component({
-  selector: 'app-metadata-form',
+  selector: "app-metadata-form",
   standalone: true,
   imports: [
     CommonModule,
@@ -20,16 +37,66 @@ import { QuestionComponent } from 'src/app/components/reusable/question/question
 
     MatTabsModule,
     MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
-  templateUrl: './metadata-form.component.html',
-  styleUrl: './metadata-form.component.scss'
+  templateUrl: "./metadata-form.component.html",
+  styleUrl: "./metadata-form.component.scss",
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MetadataFormComponent {
+export class MetadataFormComponent implements OnInit, AfterViewInit, DoCheck {
   @Input() metadata: Array<any>;
-  @Input() buttonAction: { label: string, action: Function };
+  @Input() buttonAction: { label: string; action: Function };
   @Output() filtersCleaned: EventEmitter<any> = new EventEmitter<any>();
 
-  @ViewChild('filter', { read: FilterComponent }) filter: FilterComponent;
+  @ViewChild("filter", { read: FilterComponent }) filter: FilterComponent;
+
+  displayedColumns: string[] = ["key", "value", "actions"];
+  dataSource: MatTableDataSource<any>;
+
+  private metadataDiffer: IterableDiffer<any>;
+
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor(
+    private _processMetadataService: ProcessMetadataService,
+    private cdr: ChangeDetectorRef,
+    private differs: IterableDiffers
+  ) {}
+
+  ngOnInit() {
+    this.metadataDiffer = this.differs.find(this.metadata).create();
+    this.dataSource = new MatTableDataSource();
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  // we need this, in order to make pagination work with *ngIf
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngDoCheck() {
+    const changes = this.metadataDiffer.diff(this.metadata);
+    if (changes) {
+      this.dataSource.data = this._processMetadataService.buildFiltersList(
+        this.metadata
+      );
+      this.cdr.markForCheck();
+    }
+  }
+
+  removeElement(element: any): void {
+    let indexMetadata = this.dataSource.data.indexOf(element);
+    this.metadata.splice(indexMetadata, 1);
+    const index = this.dataSource.data.indexOf(element);
+    if (index >= 0) {
+      this.dataSource.data.splice(index, 1);
+      this.dataSource._updateChangeSubscription();
+    }
+  }
 
   clearFilters() {
     this.filter.clearFilters();
