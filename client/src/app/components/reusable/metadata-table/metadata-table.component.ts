024958424d193b4c0971f2e1564e35c4f41aac3f
Talita Ludmila de Lima <ludmilalima@outlook.com>
2024-11-08 22:15:45 -0300

    implements object metadata visualization

---
+++ client/src/app/components/reusable/metadata-table/metadata-table.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from "@angular/common";
+import { Component, Inject, Input, OnInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MAT_DIALOG_DATA, MatDialogModule } from "@angular/material/dialog";
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+
+@Component({
+  selector: "app-metadata-table",
+  standalone: true,
+  imports: [
+    CommonModule,
+
+    MatTableModule,
+    MatDialogModule,
+    MatDialogModule,
+    MatButtonModule,
+  ],
+  templateUrl: "./metadata-table.component.html",
+  styleUrl: "./metadata-table.component.scss",
+})
+export class MetadataTableComponent implements OnInit {
+  metadata: Array<{ key: string; value: any }> = [];
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  dataSource: MatTableDataSource<any>;
+  displayedColumns: string[] = ["chave", "valor"];
+
+  ngOnInit(): void {
+    this.metadata = Object.entries(this.data.metadata).map(([key, value]) => ({
+      key,
+      value,
+    }));
+    this.dataSource = new MatTableDataSource(this.metadata);
+  }
+}
