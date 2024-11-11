166ba0b4848ae382be12359dc7f013d15cd08775
Talita Ludmila de Lima <ludmilalima@outlook.com>
2024-11-11 10:25:24 -0300

    include Raletion metadata

--- client/src/app/core/models/metadata/obaa.model.ts
+++ client/src/app/core/models/metadata/obaa.model.ts
@@ -19,7 +19,7 @@ export class Obaa {
     technical: Technical = new Technical();
     educational: Educational = new Educational();
     rights: Rights = new Rights();
-    // relation: Relation = new Relation();
+    relation: Relation = new Relation();
     // annotation: Annotation = new Annotation();
     // classification: Classification = new Classification();
     // accessibility: Accessibility = new Accessibility();
--- client/src/app/core/models/metadata/relation/relation.model.ts
+++ client/src/app/core/models/metadata/relation/relation.model.ts
@@ -1,16 +1,32 @@
-import { IdentifierType, LangStringType } from "../util.model";
+import { IdentifierType, LangStringType, NodeInfo } from "../util.model";
 import { Kind } from "./enums/kind.enum";
 
 export class Relation {
-    minOccurs: number = 0;
-    maxOccurs: number = 100;
-    kind: Kind = new Kind(0, 1);
-    resorce: ResourceType = new ResourceType;
+  nodeInfo: NodeInfo;
+  kind: Kind = new Kind(0, 1);
+  resource: ResourceType = new ResourceType();
+
+  constructor() {
+    this.nodeInfo = new NodeInfo(
+      0,
+      100,
+      "A categoria Relação agrupa as características que definem a relação entre o objeto de aprendizagem e outros objetos de aprendizagem relacionados.",
+      "root"
+    );
+  }
 }
 
 class ResourceType {
-    minOccurs: number = 0;
-    maxOccurs: number = 1;
-    identifier: IdentifierType = new IdentifierType(0, 10);
-    description: LangStringType = new LangStringType(0, 10);
+  nodeInfo: NodeInfo;
+  identifier: IdentifierType = new IdentifierType(0, 10);
+  description: LangStringType = new LangStringType(0, 10);
+
+  constructor() {
+    this.nodeInfo = new NodeInfo(
+      0,
+      1,
+      "O objeto de aprendizagem alvo que esta relação referencia.",
+      "root"
+    );
+  }
 }
