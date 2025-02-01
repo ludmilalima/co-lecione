58eb11da5a46a169da1703bcf3dea28c654b2764
Talita Ludmila de Lima <ludmilalima@outlook.com>
2025-02-02 00:44:29 +0100

    cria estrutura inicial da pagina welcome

--- client/src/app/features/welcome/welcome.component.ts
+++ client/src/app/features/welcome/welcome.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { MatGridListModule } from "@angular/material/grid-list";
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
-  selector: 'app-welcome',
+  selector: "app-welcome",
   standalone: true,
-  imports: [],
-  templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  imports: [CommonModule, MatGridListModule],
+  templateUrl: "./welcome.component.html",
+  styleUrl: "./welcome.component.scss",
 })
 export class WelcomeComponent {
-
+  tiles: Tile[] = [
+    { text: "One", cols: 1, rows: 1, color: "lightpink" },
+    { text: "Two", cols: 2, rows: 1, color: "#DDBDF1" },
+    { text: "Three", cols: 1, rows: 5, color: "lightgreen" },
+    { text: "Four", cols: 3, rows: 4, color: "lightblue" },
+  ];
 }
