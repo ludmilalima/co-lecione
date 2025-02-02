b6e92c0e6a403231c760aca765e06525b55a13b0
Talita Ludmila de Lima <ludmilalima@outlook.com>
2025-02-02 11:15:22 +0100

    create structure to welcome page

--- client/src/app/features/welcome/welcome.component.ts
+++ client/src/app/features/welcome/welcome.component.ts
@@ -6,7 +6,6 @@ export interface Tile {
   color: string;
   cols: number;
   rows: number;
-  text: string;
 }
 
 @Component({
@@ -18,9 +17,8 @@ export interface Tile {
 })
 export class WelcomeComponent {
   tiles: Tile[] = [
-    { text: "One", cols: 1, rows: 1, color: "lightpink" },
-    { text: "Two", cols: 2, rows: 1, color: "#DDBDF1" },
-    { text: "Three", cols: 1, rows: 5, color: "lightgreen" },
-    { text: "Four", cols: 3, rows: 4, color: "lightblue" },
+    { cols: 1, rows: 1, color: "lightpink" },
+    { cols: 3, rows: 1, color: "#DDBDF1" },
+    { cols: 4, rows: 4, color: "lightblue" },
   ];
 }
