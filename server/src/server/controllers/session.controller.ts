8257a7a5da799eceb55546aa5a4d2bc3de7aa9f3
Talita L. Lima <ludmilalima@outlook.com>
2025-07-10 09:29:18 +0000

    includes cookie token validation in request middleware interceptor

--- server/src/server/controllers/session.controller.ts
+++ server/src/server/controllers/session.controller.ts
@@ -4,19 +4,31 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 // Função para verificar a validade do token JWT
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
+    const cookieToken = req.cookies.auth_token;
 
-    if (!token) {
+    if (!token && !cookieToken) {
         res.status(401).send('Token de autenticação não fornecido.');
     }
 
-    jwt.verify(token, 'secret_key', (err: any, decoded: jwt.JwtPayload) => {
-        if (err || typeof decoded === 'string') {
-            res.status(401).send('Token de autenticação inválido.');
-        }
-        // Decodifica o token e adiciona o ID do usuário à requisição
-        req.body = {userId: (decoded as JwtPayload).userId};
+    if (token) {
+        jwt.verify(token, 'secret_key', (err: any, decoded: jwt.JwtPayload) => {
+            if (err || typeof decoded === 'string') {
+                res.status(401).send('Token de autenticação inválido.');
+            }
+            // Decodifica o token e adiciona o ID do usuário à requisição
+            req.body = { userId: (decoded as JwtPayload).userId };
+        });
+    }
 
-    });
+    if (cookieToken) {
+        try {
+            const decoded = jwt.verify(cookieToken, process.env.AUTH_SECRET_KEY);
+            req.body = {email: (decoded as JwtPayload).email}
+            res.json({ message: `Usuário ${(decoded as JwtPayload).email} logou com sucesso.` });
+        } catch {
+            res.status(401).json({ message: 'Cookie token inválido ou expirado' });
+        }
+    }
     next();
 };
 
