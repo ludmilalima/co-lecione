777f676bb804c300c20d6e537ff2e41d5ca4fa75
Talita L. Lima <ludmilalima@outlook.com>
2025-07-14 12:11:44 +0000

    updates the middleware to deal with CookieOnly authentication method

--- server/src/server/controllers/session.controller.ts
+++ server/src/server/controllers/session.controller.ts
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
@@ -7,25 +8,25 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) => 
     const cookieToken = req.cookies.auth_token;
 
     if (!token && !cookieToken) {
-        res.status(401).send('Token de autenticação não fornecido.');
+        return res.status(401).send('Token de autenticação não fornecido.');
     }
 
-    if (token) {
+    if (token == 'CookieOnly') {
+        req.body = { userId: 'CookieOnly' };
+    } else if (token) {
         jwt.verify(token, 'secret_key', (err: any, decoded: jwt.JwtPayload) => {
             if (err || typeof decoded === 'string') {
-                res.status(401).send('Token de autenticação inválido.');
+                return res.status(401).send('Token de autenticação inválido.');
             }
-            // Decodifica o token e adiciona o ID do usuário à requisição
-            req.body = { userId: (decoded as JwtPayload).userId };
         });
     }
 
     if (cookieToken) {
         try {
             const decoded = jwt.verify(cookieToken, process.env.AUTH_SECRET_KEY);
-            req.body = {email: (decoded as JwtPayload).email}
+            req.body = { email: (decoded as JwtPayload).email }
         } catch {
-            res.status(401).json({ message: 'Cookie token inválido ou expirado' });
+            return res.status(401).json({ message: 'Cookie token inválido ou expirado' });
         }
     }
     next();
